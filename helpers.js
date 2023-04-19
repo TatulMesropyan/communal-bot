9693f209c0f352183de7b2349df9a3106b16009e
t.mesropyan <Tatul.Mesropyan-extern@invia.de>
2023-04-19 20:55:54 +0400

    Prettier

--- helpers.js
+++ helpers.js
@@ -1,36 +1,43 @@
-import {getRequest} from "./server.js";
+import { getRequest } from "./server.js";
 
 export const mapCommunalTypeForRequest = (type) => {
-	let value;
-	switch (type) {
-		case 'Բոլոր Կոմունալներ': {
-			return value = 'overall'
-		}
-		case 'Գազ' : {
-			return value = 'gas'
-		}
-		case 'Ջուր' : {
-			return value = 'water'
-		}
-		case 'Հոսանք' : {
-			return value = 'el'
-		}
-		default: return value = null
-	}
+  let value;
+  switch (type) {
+    case "Բոլոր Կոմունալներ": {
+      return (value = "overall");
+    }
+    case "Գազ": {
+      return (value = "gas");
+    }
+    case "Ջուր": {
+      return (value = "water");
+    }
+    case "Հոսանք": {
+      return (value = "el");
+    }
+    default:
+      return (value = null);
+  }
 };
 
 export const someRequest = async (initials, communalType, phone) => {
-	const firstName = initials.split(' ')[0]
-	const lastName = initials.split(' ')[1]
-	const type = mapCommunalTypeForRequest(communalType)
-	//Should contain request to DB for getting data
-	const result = await getRequest(firstName, lastName, type, phone);
-	if (result) {
-		return result;
-	} else return null
+  const firstName = initials.split(" ")[0];
+  const lastName = initials.split(" ")[1];
+  const type = mapCommunalTypeForRequest(communalType);
+  //Should contain request to DB for getting data
+  const result = await getRequest(firstName, lastName, type, phone);
+  if (result) {
+    return result;
+  } else return null;
 };
 
-export const CREDENTIALS_REQUEST_TEXT = 'Խնդրում ենք գրեք ձեր անուն ազգանոնը այսպես - Հակոբ Հակոբյան';
-export const PHONE_REQUEST = 'Նշեք հեռախոսահամարը առանց +374 և 0';
-export const WRONG = 'Տվյալները սխալ են, խնդրում ենք փորձել նորից';
-export const OPTIONS = [{ text: 'Բոլոր Կոմունալներ'},{ text: 'Գազ'}, { text: 'Հոսանք'},{ text: 'Ջուր'}];
+export const CREDENTIALS_REQUEST_TEXT =
+  "Խնդրում ենք գրեք ձեր անուն ազգանոնը այսպես - Հակոբ Հակոբյան";
+export const PHONE_REQUEST = "Նշեք հեռախոսահամարը առանց +374 և 0";
+export const WRONG = "Տվյալները սխալ են, խնդրում ենք փորձել նորից";
+export const OPTIONS = [
+  { text: "Բոլոր Կոմունալներ" },
+  { text: "Գազ" },
+  { text: "Հոսանք" },
+  { text: "Ջուր" },
+];
--- server.js
+++ server.js
@@ -1,25 +1,23 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
 const MONGO_URI = process.env.SCHEMA_URL;
 const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
 
 export async function getRequest(credentials, type, phone) {
-	const client = new MongoClient(MONGO_URI, { useUnifiedTopology: true });
+  const client = new MongoClient(MONGO_URI, { useUnifiedTopology: true });
 
-	try {
-		await client.connect();
-		const database = client.db(MONGO_DB_NAME);
-		const collection = database.collection('requests');
+  try {
+    await client.connect();
+    const database = client.db(MONGO_DB_NAME);
+    const collection = database.collection("requests");
 
-		const query = { credentials, type, phone };
-		const result = await collection.findOne(query);
+    const query = { credentials, type, phone };
+    const result = await collection.findOne(query);
 
-		return result;
-	} catch (err) {
-		console.log(err);
-	} finally {
-		await client.close();
-	}
+    return result;
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await client.close();
+  }
 }
-
-
